b8fe797cf0883fe311005e8132beae357cc488e9
rendrasc <rendrasc@yahoo.com>
2026-01-11 16:56:44 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/csms/',
+  base: "/csms/",  // <--- IMPORTANT: This must match your repo name exactly
 })
